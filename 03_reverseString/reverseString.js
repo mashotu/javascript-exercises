15d0637ce046c1b3bd079db2d6c945f094bcc2a1
mashotu <155717478+mashotu@users.noreply.github.com>
2025-04-09 14:48:42 -0400

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let arr = [];
+    for (let i = 0, j = string.length; i < j; i++) {
+        arr.push(string.charAt(i));
+    }
 
+    return (arr.reverse()).join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
