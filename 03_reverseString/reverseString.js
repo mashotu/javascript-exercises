9ce20a4723313711e4bc4edf5266da56a86adf90
mashotu <155717478+mashotu@users.noreply.github.com>
2025-04-09 14:59:38 -0400

    Simplify reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function (string) {
-    let arr = [];
-    for (let i = 0, j = string.length; i < j; i++) {
-        arr.push(string.charAt(i));
-    }
-
-    return (arr.reverse()).join("");
+    return string.split("").reverse().join("");
 };
 
 
