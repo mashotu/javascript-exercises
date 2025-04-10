18e10013f6a86ccf1fcbdeb497faa72027c0a034
mashotu <155717478+mashotu@users.noreply.github.com>
2025-04-10 12:19:21 -0400

    Complete tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  return parseFloat((((fahr - 32) * 5) / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  return parseFloat(((cel * 9) / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
