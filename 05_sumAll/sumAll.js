1f05ab966e3523d13a391b3e92322b12168d487d
mashotu <155717478+mashotu@users.noreply.github.com>
2025-04-10 11:51:07 -0400

    Complete sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
+  if (
+    [a, b].some((n) => typeof n !== "number" || n < 0 || !Number.isInteger(n))
+  ) {
+    return "ERROR";
+  }
+
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
